dda38b65041c447bb5680fb290448132d065b646
sayden <mariocaster@gmail.com>
2016-05-08 19:02:42 +0200

    binary_counter linted

--- binary_counter/binary_counter_tb.v
+++ binary_counter/binary_counter_tb.v
@@ -3,8 +3,10 @@ module binary_counter_tb;
 	// Inputs
 	reg clk ;
 	reg reset;
+
 	// Output
 	wire[3:0] binary;
+
 	// Instantiate the Binary Counter
 	binary_counter  #(4) s1  (
 		.clk(clk),
@@ -12,31 +14,29 @@ module binary_counter_tb;
 		.binary(binary)
 	);
 
- integer i;
- initial
- begin
+  integer i;
+  initial begin
 
- clk = 0;
- for(i =0; i<=40; i=i+1)
- begin
-  #10 clk = ~clk;
- end
- end
+	clk = 0;
+	for(i =0; i<=40; i=i+1)
+		begin
+			#10 clk = ~clk;
+		end
+	end
 
-initial
-begin
+	initial begin
 
-$dumpfile("binary_counter_tb.vcd");
-$dumpvars(0,binary_counter_tb);
+		$dumpfile("binary_counter_tb.vcd");
+		$dumpvars(0, binary_counter_tb);
 
-reset =1;
-#2 reset = 0;
-#2 reset =1;
-end
+		reset = 1;
+		#2 reset = 0;
+		#2 reset =1;
+	end
 
 
-		initial begin
-		 $monitor("clk=%d binary=%4b",clk,binary);
-		 end
+	initial begin
+	  $monitor("clk=%d binary=%4b",clk,binary);
+ 	end
 
 endmodule
